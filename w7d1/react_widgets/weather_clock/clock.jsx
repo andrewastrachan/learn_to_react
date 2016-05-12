ddf7ac7b794081c4435d4f7932f2306c2f3dc924
Andrew Strachan <andrewastrachan@outlook.com>
2016-05-11 19:30:18 -0700

    do the clock

--- w7d1/react_widgets/weather_clock/clock.jsx
+++ w7d1/react_widgets/weather_clock/clock.jsx
@@ -1,8 +1,18 @@
 var React = require('react');
 
 var Clock = React.createClass({
+  getInitialState: function() {
+    return ({currentTime: (new Date).toLocaleTimeString()})
+  },
+
+  componentDidMount: function() {
+    setInterval(function() {
+      this.setState({currentTime: (new Date).toLocaleTimeString()})
+    }.bind(this), 1000)
+  },
+
   render: function() {
-    return <div>Clock Goes Here</div>
+    return <div>Current Time: {this.state.currentTime}</div>
   }
 });
 
