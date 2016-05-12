53333ce62dbd680c587d2ffd00843a1e66c6bb8a
Andrew Strachan <andrewastrachan@outlook.com>
2016-05-11 18:33:38 -0700

    pass in data to widgets component

--- w7d1/react_widgets/widgets.jsx
+++ w7d1/react_widgets/widgets.jsx
@@ -5,28 +5,28 @@ var React = require('react'),
 
 var Widgets = React.createClass({
   render: function() {
-    var tabData = [
-      {title: 'tab 1', content: 'tab 1 content'},
-      {title: 'tab 2', content: 'tab 2 content'},
-      {title: 'tab 3', content: 'tab 3 content'}
-    ]
-
-    var searchItems = [
-      {name: 'Andrew Strachan'},
-      {name: 'Ben Whitley'},
-      {name: 'Christian Davis'},
-      {name: 'Edward McDonald'}
-    ]
-
     return (
       <div>
-        <Tabs tabs={tabData} />
-        <Autocomplete searchItems={searchItems} />
+        <Tabs tabs={this.props.tabData} />
+        <Autocomplete searchItems={this.props.searchItems} />
       </div>
     )
   }
 });
 
+var tabData = [
+  {title: 'tab 1', content: 'tab 1 content'},
+  {title: 'tab 2', content: 'tab 2 content'},
+  {title: 'tab 3', content: 'tab 3 content'}
+]
+
+var searchItems = [
+  {name: 'Andrew Strachan'},
+  {name: 'Ben Whitley'},
+  {name: 'Christian Davis'},
+  {name: 'Edward McDonald'}
+]
+
 document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(<Widgets />, document.getElementById('main'));
+  ReactDOM.render(<Widgets tabData={tabData} searchItems={searchItems} />, document.getElementById('main'));
 });
