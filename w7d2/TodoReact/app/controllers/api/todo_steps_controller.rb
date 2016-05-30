4d8ebb484af915801f3bcda64d6113c802b257b4
Andrew Strachan <andrewastrachan@outlook.com>
2016-05-30 11:34:12 -0700

    todo step contoller inital pass

--- w7d2/TodoReact/app/controllers/api/todo_steps_controller.rb
+++ w7d2/TodoReact/app/controllers/api/todo_steps_controller.rb
@@ -1,18 +1,25 @@
 class Api::TodoStepsController < ApplicationController
   before_action :find_todo
   def index
+    render json: @todo.todo_steps.to_json
   end
 
   def show
+    render json: @todo.todo_steps.find(params[:id]).to_json
   end
 
   def create
+    render json: @todo.todo_steps.create!(todo_step_params).to_json
   end
 
   def update
+    todo_step = @todo.todo_steps.find(params[:id])
+    todo_step.update_attributes!(todo_step_params)
+    render json: todo_step.to_json
   end
 
   def destroy
+    render json: @todo.todo_steps.find(params[:id]).destroy!.to_json
   end
 
   protected
